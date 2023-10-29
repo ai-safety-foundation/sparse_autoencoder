d986b2bd52dc9d5ac5966f8cbb54ebef6f56084c
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-10-29 15:53:26 +0800

    Add the sparse autoencoder model

---
+++ sparse_autoencoder/autoencoder/model.py
@@ -0,0 +1,74 @@
+"""The Sparse Autoencoder Model."""
+import torch
+from jaxtyping import Float
+from torch import Tensor
+from torch.nn import Linear, Module, ReLU, Sequential
+from torch.nn.parameter import Parameter
+
+from sparse_autoencoder.autoencoder.tied_bias import PostEncoderBias, PreEncoderBias
+
+
+class SparseAutoencoder(Module):
+    """Sparse Autoencoder Model.
+
+    Args:
+        n_input_features: Number of input features (e.g. `d_mlp` if training on MLP activations from
+            TransformerLens).
+        n_learned_features: Number of learned features. The initial paper experimented with 1× to
+            256× the number of input features, and primarily used 8x.
+        geometric_median_dataset: Estimated geometric median of the dataset.
+    """
+
+    geometric_median_dataset: Float[Tensor, "input_activations"]
+    """Estimated Geometric Median of the Dataset.
+    
+    Used for initialising :attr:`tied_bias`.
+    """
+
+    tied_bias: Float[Parameter, "input_activations"]
+    """Tied Bias Parameter.
+    
+    The same bias is used pre-encoder and post-decoder.
+    """
+
+    def __init__(
+        self,
+        n_input_features: int,
+        n_learned_features: int,
+        geometric_median_dataset: Float[Tensor, "input_activations"],
+    ) -> None:
+        super().__init__()
+
+        # Store the geometric median of the dataset (so that we can reset parameters). This is not a
+        # parameter itself (the tied bias parameter is used for that), so gradients are disabled.
+        self.geometric_median_dataset = geometric_median_dataset.clone()
+        self.geometric_median_dataset.requires_grad = False
+
+        # Initialize the tied bias
+        self.tied_bias = Parameter(torch.empty(n_input_features))
+        self.initialize_tied_parameters()
+
+        # Create the network
+        self.network = Sequential(
+            PreEncoderBias(self.tied_bias),
+            Linear(n_input_features, n_learned_features),
+            ReLU(),
+            Linear(n_learned_features, n_input_features),
+            PostEncoderBias(self.tied_bias),
+        )
+
+    def forward(self, input: Tensor) -> Tensor:
+        """Forward Pass."""
+        return self.network(input)
+
+    def initialize_tied_parameters(self) -> None:
+        """Initialize the tied parameters."""
+        # The tied bias is initialised as the geometric median of the dataset
+        self.tied_bias.data = self.geometric_median_dataset.clone()
+
+    def reset_parameters(self) -> None:
+        """Reset the parameters."""
+        self.initialize_tied_parameters()
+        for module in self.network:
+            if "reset_parameters" in dir(module):
+                module.reset_parameters()
---
+++ sparse_autoencoder/autoencoder/tests/test_model.py
@@ -0,0 +1,21 @@
+"""Sparse Autoencoder Model Tests."""
+import torch
+
+from sparse_autoencoder.autoencoder.model import SparseAutoencoder
+
+
+def test_initialize_tied_bias():
+    """Check the tied bias is initialised correctly."""
+    geometric_median = torch.tensor([1.0, 2.0, 3.0])
+    model = SparseAutoencoder(3, 6, geometric_median)
+    assert torch.allclose(model.tied_bias, geometric_median)
+
+
+def test_encoded_decoded_shape_same():
+    """Check the input and output are the same shape."""
+    geometric_median = torch.tensor([1.0, 2.0, 3.0])
+    model = SparseAutoencoder(3, 6, geometric_median)
+    input_tensor = torch.tensor([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]])
+    output = model(input_tensor)
+
+    assert output.shape == input_tensor.shape
