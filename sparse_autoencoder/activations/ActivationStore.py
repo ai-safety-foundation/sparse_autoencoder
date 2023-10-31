ab8dab0ab6759342a6d6d733c5b8e88bdbc05166
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-01 06:44:44 +0800

    Enforce that AbstractStore is extended
    It's an abstract class

--- sparse_autoencoder/activations/ActivationStore.py
+++ sparse_autoencoder/activations/ActivationStore.py
@@ -1,5 +1,5 @@
 """Activation Store Base Class."""
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 from jaxtyping import Float
 from torch import Tensor
@@ -12,13 +12,17 @@ ActivationStoreBatch = Float[Tensor, "batch neuron"]
 """Activation Store Dataset Batch Type."""
 
 
-class ActivationStore(Dataset):
+class ActivationStore(Dataset, ABC):
     """Activation Store Abstract Class.
 
     Extends the `torch.utils.data.Dataset` class to provide an activation store, with additional
-    :meth:`append` and :meth:`extend` methods (the latter of which should be non-blocking).
+    :meth:`append` and :meth:`extend` methods (the latter of which should typically be
+    non-blocking).
 
-    Can be used with a `torch.utils.data.DataLoader` to iterate over the dataset.
+    Extend this class if you want to create a new activation store.
+
+    The resulting activation store can be used with a `torch.utils.data.DataLoader` to iterate over
+    the dataset.
     """
 
     @abstractmethod
