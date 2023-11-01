4fd34a64db65bc5a575520f7e4af8c215fbe6aeb
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-01 20:50:41 +0800

    Add hook to store just the activations we want

--- sparse_autoencoder/src_model/store_activations_hook.py
+++ sparse_autoencoder/src_model/store_activations_hook.py
@@ -1,3 +1,4 @@
+"""TransformerLens Hook for storing activations."""
 from jaxtyping import Float
 from torch import Tensor
 from transformer_lens.hook_points import HookPoint
@@ -6,8 +7,38 @@ from sparse_autoencoder.activation_store.base_store import ActivationStore
 
 
 def store_activations_hook(
-    value: Float[Tensor, "*batch_and_pos neuron"],
-    hook: HookPoint,
+    value: Float[Tensor, "*any neuron"],
+    hook: HookPoint,  # pylint: disable=unused-argument
     store: ActivationStore,
 ):
-    pass
+    """Store Activations Hook.
+
+    Useful for getting just the specific activations wanted, rather than the full cache.
+
+    Example:
+
+    First we'll need a source model from TransformerLens and an activation store.
+
+    >>> from functools import partial
+    >>> from transformer_lens import HookedTransformer
+    >>> from sparse_autoencoder.activation_store.list_store import ListActivationStore
+    >>> store = ListActivationStore()
+    >>> model = HookedTransformer.from_pretrained("tiny-stories-1M")
+    Loaded pretrained model tiny-stories-1M into HookedTransformer
+
+    Next we can add the hook to specific neurons (in this case the first MLP neurons), and do a
+    forward pass.
+
+    >>> model.add_hook(
+    ...     "blocks.0.mlp.hook_post", partial(store_activations_hook, store=store)
+    ... )
+    >>> _logits = model.forward("Hello world") # "Hello world" is 3 tokens
+    >>> len(store)
+    3
+
+    Args:
+        value: The activations to store.
+        hook: The hook point.
+        store: The activation store. This should be pre-initialised with `functools.partial`.
+    """
+    store.extend(value)
---
+++ sparse_autoencoder/src_model/tests/test_store_activations_hook.py
@@ -0,0 +1,26 @@
+"""Store Activations Hook Tests."""
+from functools import partial
+
+from transformer_lens import HookedTransformer
+
+from sparse_autoencoder.activation_store.list_store import ListActivationStore
+from sparse_autoencoder.src_model.store_activations_hook import store_activations_hook
+
+
+def test_hook_stores_activations():
+    """Test that the hook stores activations correctly."""
+    store = ListActivationStore()
+    model = HookedTransformer.from_pretrained("tiny-stories-1M")
+
+    model.add_hook(
+        "blocks.0.mlp.hook_post", partial(store_activations_hook, store=store)
+    )
+
+    tokens = model.to_tokens("Hello world")
+    model.forward(tokens)
+
+    number_of_tokens = tokens.numel()
+    mlp_size: int = model.cfg.d_mlp
+
+    assert len(store) == number_of_tokens
+    assert store[0].shape[0] == mlp_size
