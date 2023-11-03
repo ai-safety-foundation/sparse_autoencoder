03bfbe564e842a511528f2ff6cde2dc90e64ff44
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-03 09:19:10 +0800

    Enable dict representation of sweep config

--- sparse_autoencoder/train/sweep_config.py
+++ sparse_autoencoder/train/sweep_config.py
@@ -1,8 +1,9 @@
 """Sweep Config."""
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
 
 from sparse_autoencoder.train.utils.wandb_sweep_types import (
     Method,
+    Metric,
     Parameter,
     Parameters,
     WandbSweepConfig,
@@ -14,44 +15,36 @@ from sparse_autoencoder.train.utils.wandb_sweep_types import (
 class SweepParameterConfig(Parameters):
     """Sweep Parameter Config."""
 
-    lr: Parameter[float] = field(default_factory=lambda: Parameter(value=0.001))
+    lr: Parameter[float] | None
     """Adam Learning Rate."""
 
-    adam_beta_1: Parameter[float] = field(default_factory=lambda: Parameter(value=0.9))
+    adam_beta_1: Parameter[float] | None
     """Adam Beta 1.
     
     The exponential decay rate for the first moment estimates (mean) of the gradient.
     """
 
-    adam_beta_2: Parameter[float] = field(
-        default_factory=lambda: Parameter(value=0.999)
-    )
+    adam_beta_2: Parameter[float] | None
     """Adam Beta 2.
     
     The exponential decay rate for the second moment estimates (variance) of the gradient.
     """
 
-    adam_epsilon: Parameter[float] = field(
-        default_factory=lambda: Parameter(value=1e-8)
-    )
+    adam_epsilon: Parameter[float] | None
     """Adam Epsilon.
     
     A small constant for numerical stability.
     """
 
-    adam_weight_decay: Parameter[float] = field(
-        default_factory=lambda: Parameter(value=0)
-    )
+    adam_weight_decay: Parameter[float] | None
     """Adam Weight Decay.
     
     Weight decay (L2 penalty).
     """
 
-    batch_size: Parameter[int] = field(default_factory=lambda: Parameter(value=8192))
+    batch_size: Parameter[int] | None
 
-    l1_coefficient: Parameter[float] = field(
-        default_factory=lambda: Parameter(value=[0.001, 0.004, 0.006, 0.008, 1])
-    )
+    l1_coefficient: Parameter[float] | None
     """L1 Penalty Coefficient.
     
     The L1 penalty is the absolute sum of learned (hidden) activations, multiplied by this constant.
@@ -68,19 +61,23 @@ class SweepParameterConfig(Parameters):
 class SweepParametersRuntime(dict):
     """Sweep parameter runtime values."""
 
-    lr: float
+    lr: float = 0.001
 
-    adam_beta_1: float
+    adam_beta_1: float = 0.9
 
-    adam_beta_2: float
+    adam_beta_2: float = 0.999
 
-    adam_epsilon: float
+    adam_epsilon: float = 1e-8
 
-    adam_weight_decay: float
+    adam_weight_decay: float = 0.0
 
-    batch_size: int
+    batch_size: int = 8
 
-    l1_coefficient: float
+    l1_coefficient: float = 0.01
+
+    def to_dict(self):
+        """Return dict representation of this object."""
+        return asdict(self)
 
 
 @dataclass
@@ -89,4 +86,32 @@ class SweepConfig(WandbSweepConfig):
 
     parameters: SweepParameterConfig
 
-    method: Method = Method.bayes
+    method: Method = Method.grid
+
+    metric: Metric = field(default_factory=lambda: Metric(name="loss"))
+
+    def to_dict(self):
+        """Return dict representation of this object."""
+        dict_representation = asdict(self)
+
+        # Convert StrEnums to strings
+        dict_representation["method"] = dict_representation["method"].value
+
+        def remove_none_values(d):
+            """Recursively remove all None values from the dictionary."""
+            if isinstance(d, dict):
+                return {
+                    k: remove_none_values(v)
+                    for k, v in d.items()
+                    if v is not None and remove_none_values(v) is not None
+                }
+            elif isinstance(d, list):
+                return [
+                    remove_none_values(v)
+                    for v in d
+                    if v is not None and remove_none_values(v) is not None
+                ]
+            else:
+                return d
+
+        return remove_none_values(dict_representation)
