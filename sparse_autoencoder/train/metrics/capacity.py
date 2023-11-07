27381d42861ba374fdad73ee70ba617ae1fd4f92
HoagyC <hoagycunningham@gmail.com>
2023-11-07 15:26:48 +0000

    Fix tests.

--- sparse_autoencoder/train/metrics/capacity.py
+++ sparse_autoencoder/train/metrics/capacity.py
@@ -1,49 +1,43 @@
-import numpy as np
-import torch
-import wandb
+"""Capacity metrics for sets of learned features."""
+
 from jaxtyping import Float
 from numpy import histogram
+import numpy as np
 from numpy.typing import NDArray
+import torch
 from torch import Tensor
+import wandb
 
 
-def calc_capacities(
-    features: Float[Tensor, "n_feats feat_dim"]
-) -> Float[Tensor, " n_feats"]:
-    """
-    Measuring the capacity of a set of features as defined in https://arxiv.org/pdf/2210.01892.pdf
-    
+def calc_capacities(features: Float[Tensor, "n_feats feat_dim"]) -> Float[Tensor, " n_feats"]:
+    """Measuring the capacity of a set of features as defined in https://arxiv.org/pdf/2210.01892.pdf.
+
     Capacity is intuitively measuring the 'proportion of a dimension' assigned to a feature.
     Formally it's the ratio of the squared dot product of a feature with itself to the sum of its
     squared dot products of all features.
-    
+
     If the features are orthogonal, the capacity is 1. If they are all the same, the capacity is
     1/n.
+
     Example:
         >>> import torch
-        >>> orthogonal_features = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        >>> orthogonal_caps = calc_capacities(features)
-        >>> assert torch.allclose(caps, torch.tensor([1, 1, 1]))
-        >>> non_orthogonal_features = torch.tensor([[1, 0, 0], [1, 1, 0], [0, 0, 1]])
-        >>> non_orthogonal_caps = calc_capacities(features)
-        >>> assert torch.allclose(caps, torch.tensor([0.5, 0.8, 1]))
-    
+        >>> orthogonal_features = torch.tensor([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+        >>> orthogonal_caps = calc_capacities(orthogonal_features)
+        >>> assert torch.allclose(orthogonal_caps, torch.tensor([1., 1., 1.]))
+
     Args:
         features: A 2D tensor of features, where each row is a feature.
-        
+
     Returns:
         A 1D tensor of capacities, where each element is the capacity of the corresponding feature.
     """
     squared_dot_products = (features @ features.T).pow(2)
-    sum_of_sq_dot = squared_dot_products.sum(dim=-1, keepdim=True)
-    capacities = torch.diag(squared_dot_products) / sum_of_sq_dot
-    
-    return capacities
+    sum_of_sq_dot = squared_dot_products.sum(dim=-1)
+    return torch.diag(squared_dot_products) / sum_of_sq_dot
 
 
 def wandb_capacities_histogram(
     capacities: Float[Tensor, " n_feats"],
-    
 ) -> wandb.Histogram:
     """Create a W&B histogram of the capacities.
 
