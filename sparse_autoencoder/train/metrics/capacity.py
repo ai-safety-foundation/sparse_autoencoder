42cc1a2d15cc2911eeb704e2b9cdbf412d218a91
HoagyC <hoagycunningham@gmail.com>
2023-11-07 14:55:32 +0000

    Add capacity metric.

---
+++ sparse_autoencoder/train/metrics/capacity.py
@@ -0,0 +1,62 @@
+import numpy as np
+import torch
+import wandb
+from jaxtyping import Float
+from numpy import histogram
+from numpy.typing import NDArray
+from torch import Tensor
+
+
+def calc_capacities(
+    features: Float[Tensor, "n_feats feat_dim"]
+) -> Float[Tensor, " n_feats"]:
+    """
+    Measuring the capacity of a set of features as defined in https://arxiv.org/pdf/2210.01892.pdf
+    
+    Capacity is intuitively measuring the 'proportion of a dimension' assigned to a feature.
+    Formally it's the ratio of the squared dot product of a feature with itself to the sum of its
+    squared dot products of all features.
+    
+    If the features are orthogonal, the capacity is 1. If they are all the same, the capacity is
+    1/n.
+    Example:
+        >>> import torch
+        >>> orthogonal_features = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        >>> orthogonal_caps = calc_capacities(features)
+        >>> assert torch.allclose(caps, torch.tensor([1, 1, 1]))
+        >>> non_orthogonal_features = torch.tensor([[1, 0, 0], [1, 1, 0], [0, 0, 1]])
+        >>> non_orthogonal_caps = calc_capacities(features)
+        >>> assert torch.allclose(caps, torch.tensor([0.5, 0.8, 1]))
+    
+    Args:
+        features: A 2D tensor of features, where each row is a feature.
+        
+    Returns:
+        A 1D tensor of capacities, where each element is the capacity of the corresponding feature.
+    """
+    squared_dot_products = (features @ features.T).pow(2)
+    sum_of_sq_dot = squared_dot_products.sum(dim=-1, keepdim=True)
+    capacities = torch.diag(squared_dot_products) / sum_of_sq_dot
+    
+    return capacities
+
+
+def wandb_capacities_histogram(
+    capacities: Float[Tensor, " n_feats"],
+    
+) -> wandb.Histogram:
+    """Create a W&B histogram of the capacities.
+
+    This can be logged with Weights & Biases using e.g. `wandb.log({"capacities_histogram":
+    wandb_capacities_histogram(capacities)})`.
+
+    Args:
+        capacities: Capacity of each feature. Can be calculated using :func:`calc_capacities`.
+
+    Returns:
+        Weights & Biases histogram for logging with `wandb.log`.
+    """
+    numpy_capacities: NDArray[np.float_] = capacities.detach().cpu().numpy()
+
+    bins, values = histogram(numpy_capacities, bins=20, range=(0, 1))
+    return wandb.Histogram(np_histogram=(bins, values))
