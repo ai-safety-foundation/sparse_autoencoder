29ff15af6a090cc3d9b0090d8c34c0ce226f4373
Alan <41682961+alan-cooney@users.noreply.github.com>
2023-11-08 00:22:11 +0800

    Fix demo notebook (#50)

--- sparse_autoencoder/source_data/tests/test_pile_uncopyrighted.py
+++ sparse_autoencoder/source_data/tests/test_pile_uncopyrighted.py
@@ -21,3 +21,22 @@ def test_tokenized_prompts_correct_size(context_size: int) -> None:
         # Check the tokens are integers
         for token in item["input_ids"]:
             assert isinstance(token, int)
+
+
+def test_dataloader_correct_size_items() -> None:
+    """Test the dataloader returns the correct number & sized items."""
+    batch_size = 10
+    context_size = 250
+    tokenizer = PreTrainedTokenizerFast.from_pretrained("gpt2")
+    data = PileUncopyrightedDataset(tokenizer=tokenizer, context_size=context_size)
+    dataloader = data.get_dataloader(batch_size=batch_size)
+
+    checks = 100
+    for item in dataloader:
+        checks -= 1
+        if checks == 0:
+            break
+
+        tokens = item["input_ids"]
+        assert tokens.shape[0] == batch_size
+        assert tokens.shape[1] == context_size
