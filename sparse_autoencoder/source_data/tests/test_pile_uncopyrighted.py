cb36cc0186419aba49e5a7d38c7c1d0c66c10419
Alan <41682961+alan-cooney@users.noreply.github.com>
2023-11-07 15:54:55 +0800

    Add Pile Uncopyrighted source dataset (#36)

---
+++ sparse_autoencoder/source_data/tests/test_pile_uncopyrighted.py
@@ -0,0 +1,19 @@
+"""Pile Uncopyrighted Dataset Tests."""
+import pytest
+from transformers import PreTrainedTokenizerFast
+
+from sparse_autoencoder.source_data.pile_uncopyrighted import PileUncopyrightedDataset
+
+
+@pytest.mark.parametrize("context_size", [50, 250])
+def test_tokenized_prompts_correct_size(context_size: int) -> None:
+    """Test that the tokenized prompts have the correct context size."""
+    tokenizer = PreTrainedTokenizerFast.from_pretrained("gpt2")
+
+    data = PileUncopyrightedDataset(tokenizer=tokenizer, context_size=context_size)
+
+    # Check the first 100 items
+    iterable = iter(data.dataset)
+    for _ in range(100):
+        item = next(iterable)
+        assert len(item["input_ids"]) == context_size
