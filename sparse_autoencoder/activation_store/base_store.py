6072185fd80746055afd0c92f54c8c9d0fc8447d
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-08 21:53:23 +0800

    Add test data method to store

--- sparse_autoencoder/activation_store/base_store.py
+++ sparse_autoencoder/activation_store/base_store.py
@@ -1,8 +1,10 @@
 """Activation Store Base Class."""
 from abc import ABC, abstractmethod
 from concurrent.futures import Future
+from typing import final
 
 from jaxtyping import Float
+import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 
@@ -89,6 +91,36 @@ class ActivationStore(Dataset[ActivationStoreItem], ABC):
 
     def shuffle(self) -> None:
         """Optional shuffle method."""
+
+    @final
+    def fill_with_test_data(
+        self, num_batches: int = 16, batch_size: int = 16, input_features: int = 256
+    ) -> None:
+        """Fill the store with test data.
+
+        For use when testing your code, to ensure it works with a real activation store.
+
+        Warning:
+            You may want to use `torch.seed(0)` to make the random data deterministic, if your test
+            requires inspecting the data itself.
+
+        Example:
+            >>> from sparse_autoencoder.activation_store.tensor_store import TensorActivationStore
+            >>> store = TensorActivationStore(max_items=16*16, num_neurons=256)
+            >>> store.fill_with_test_data()
+            >>> len(store)
+            256
+            >>> store[0].shape
+            torch.Size([256])
+
+        Args:
+            num_batches: Number of batches to fill the store with.
+            batch_size: Number of items per batch.
+            input_features: Number of input features per item.
+        """
+        for _ in range(num_batches):
+            sample = torch.rand((batch_size, input_features))
+            self.extend(sample)
 
 
 class StoreFullError(IndexError):
