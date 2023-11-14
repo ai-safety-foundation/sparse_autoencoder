a1ac01d3b842393f9f312b1e79597d09909d277d
Joseph Bloom <jbloomaus@gmail.com>
2023-11-14 11:38:11 +0000

    add pretokenized dataset

---
+++ sparse_autoencoder/source_data/pretokenized_dataset.py
@@ -0,0 +1,95 @@
+"""Pre-Tokenized Dataset from Hugging Face.
+
+PreTokenizedDataset should work with any of the following tokenized datasets:
+- NeelNanda/pile-small-tokenized-2b
+- NeelNanda/pile-tokenized-10b
+- NeelNanda/openwebtext-tokenized-9b
+- NeelNanda/c4-tokenized-2b
+- NeelNanda/code-tokenized
+- NeelNanda/c4-code-tokenized-2b
+- NeelNanda/pile-old-tokenized-2b
+
+"""
+
+from typing import TypedDict, final
+
+from sparse_autoencoder.source_data.abstract_dataset import SourceDataset, TokenizedPrompts
+
+
+class PreTokenizedDataBatch(TypedDict):
+    """General Pre-Tokenized Dataset Item.
+
+    Structure depends on the specific dataset from Hugging Face.
+    """
+
+    tokens: list[
+        list[int]
+    ]  # This assumes that the dataset structure is similar to the original Neel Nanda dataset.
+
+
+@final
+class PreTokenizedDataset(SourceDataset[PreTokenizedDataBatch]):
+    """General Pre-Tokenized Dataset from Hugging Face.
+
+    Can be used for various datasets available on Hugging Face.
+    """
+
+    def preprocess(
+        self,
+        source_batch: PreTokenizedDataBatch,
+        *,
+        context_size: int,
+    ) -> TokenizedPrompts:
+        """Preprocess a batch of prompts.
+
+        The method splits each pre-tokenized item based on the context size.
+
+        Args:
+            source_batch: A batch of source data.
+            context_size: The context size to use for tokenized prompts.
+        """
+        tokenized_prompts: list[list[int]] = source_batch["tokens"]
+
+        # Chunk each tokenized prompt into blocks of context_size,
+        # discarding the last block if too small.
+        context_size_prompts = []
+        for encoding in tokenized_prompts:
+            chunks = [
+                encoding[i : i + context_size]
+                for i in range(0, len(encoding), context_size)
+                if len(encoding[i : i + context_size]) == context_size
+            ]
+            context_size_prompts.extend(chunks)
+
+        return {"input_ids": context_size_prompts}
+
+    def __init__(
+        self,
+        dataset_path: str,
+        context_size: int = 250,
+        buffer_size: int = 1000,
+        preprocess_batch_size: int = 1000,
+        dataset_split: str = "train",
+    ):
+        """Initialize a pre-tokenized dataset from Hugging Face.
+
+        Example:
+            >>> data = PreTokenizedDataset(dataset_path="NeelNanda/c4-tokenized-2b")
+            >>> first_item = next(iter(data))
+            >>> len(first_item["input_ids"])
+            250
+
+        Args:
+            dataset_path: The path to the dataset on Hugging Face.
+            context_size: The context size for tokenized prompts.
+            buffer_size: Buffer size for shuffling the dataset.
+            preprocess_batch_size: Batch size for preprocessing.
+            dataset_split: Dataset split (e.g., `train`).
+        """
+        super().__init__(
+            dataset_path=dataset_path,
+            dataset_split=dataset_split,
+            context_size=context_size,
+            buffer_size=buffer_size,
+            preprocess_batch_size=preprocess_batch_size,
+        )
---
+++ sparse_autoencoder/source_data/tests/test_pretokenized_dataset.py
@@ -0,0 +1,43 @@
+"""Tests for General Pre-Tokenized Dataset."""
+import pytest
+
+from sparse_autoencoder.source_data.pretokenized_dataset import PreTokenizedDataset
+
+
+TEST_DATASET = "NeelNanda/c4-tokenized-2b"
+
+
+@pytest.mark.parametrize("context_size", [50, 250])
+def test_tokenized_prompts_correct_size(context_size: int) -> None:
+    """Test that the tokenized prompts have the correct context size."""
+    # Use an appropriate tokenizer and dataset path
+
+    data = PreTokenizedDataset(dataset_path=TEST_DATASET, context_size=context_size)
+
+    # Check the first 100 items
+    iterable = iter(data.dataset)
+    for _ in range(100):
+        item = next(iterable)
+        assert len(item["input_ids"]) == context_size
+
+        # Check the tokens are integers
+        for token in item["input_ids"]:
+            assert isinstance(token, int)
+
+
+def test_dataloader_correct_size_items() -> None:
+    """Test the dataloader returns the correct number & sized items."""
+    batch_size = 10
+    context_size = 250
+    data = PreTokenizedDataset(dataset_path=TEST_DATASET, context_size=context_size)
+    dataloader = data.get_dataloader(batch_size=batch_size)
+
+    checks = 100
+    for item in dataloader:
+        checks -= 1
+        if checks == 0:
+            break
+
+        tokens = item["input_ids"]
+        assert tokens.shape[0] == batch_size
+        assert tokens.shape[1] == context_size
