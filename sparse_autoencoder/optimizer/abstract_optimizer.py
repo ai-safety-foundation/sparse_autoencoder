923b801de9f6b1a2430ca8033188cab22548383b
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-16 07:38:39 -0800

    Improve optimizer documentation

--- sparse_autoencoder/optimizer/abstract_optimizer.py
+++ sparse_autoencoder/optimizer/abstract_optimizer.py
@@ -1,9 +1,16 @@
 """Abstract optimizer with reset."""
 from abc import ABC, abstractmethod
 
+from sparse_autoencoder.tensor_types import DeadNeuronIndices
+
 
 class AbstractOptimizerWithReset(ABC):
-    """Abstract optimizer with reset."""
+    """Abstract optimizer with reset.
+
+    When implementing this interface, we recommend adding a `named_parameters` argument to the
+    constructor, which can be obtained from `named_parameters=model.named_parameters()` by the end
+    user. This is so that the optimizer can find the parameters to reset.
+    """
 
     @abstractmethod
     def reset_state_all_parameters(self) -> None:
@@ -11,5 +18,30 @@ class AbstractOptimizerWithReset(ABC):
 
         Resets any optimizer state (e.g. momentum). This is for use after manually editing model
             parameters (e.g. with activation resampling).
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def reset_neurons_state(
+        self,
+        parameter_name: str,
+        neuron_indices: DeadNeuronIndices,
+        axis: int,
+        parameter_group: int = 0,
+    ) -> None:
+        """Reset the state for specific neurons, on a specific parameter.
+
+        Args:
+            parameter_name: The name of the parameter. Examples from the standard sparse autoencoder
+                implementation  include `tied_bias`, `encoder.Linear.weight`, `encoder.Linear.bias`,
+                `decoder.Linear.weight`, and `decoder.ConstrainedUnitNormLinear.weight`.
+            neuron_indices: The indices of the neurons to reset.
+            axis: The axis of the parameter to reset.
+            parameter_group: The index of the parameter group to reset (typically this is just zero,
+                unless you have setup multiple parameter groups for e.g. different learning rates
+                for different parameters).
+
+        Raises:
+            ValueError: If the parameter name is not found.
         """
         raise NotImplementedError
