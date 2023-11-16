d6a85f0e468a7c2a8f063d73ee4d2ad23d8e9694
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-11-15 23:22:44 -0800

    Add unit norm tests

--- sparse_autoencoder/autoencoder/components/tests/test_unit_norm_linear.py
+++ sparse_autoencoder/autoencoder/components/tests/test_unit_norm_linear.py
@@ -4,6 +4,49 @@ import torch
 from sparse_autoencoder.autoencoder.components.unit_norm_linear import ConstrainedUnitNormLinear
 
 
+def test_initialization() -> None:
+    """Test that the weights are initialized with unit norm."""
+    layer = ConstrainedUnitNormLinear(learnt_features=3, decoded_features=4)
+    weight_norms = torch.norm(layer.weight, dim=1)
+    assert torch.allclose(weight_norms, torch.ones_like(weight_norms))
+
+
+def test_forward_pass() -> None:
+    """Test the forward pass of the layer."""
+    layer = ConstrainedUnitNormLinear(learnt_features=3, decoded_features=4)
+    input_tensor = torch.randn(5, 3)  # Batch size of 5, learnt features of 3
+    output = layer(input_tensor)
+    assert output.shape == (5, 4)  # Batch size of 5, decoded features of 4
+
+
+def test_bias_initialization_and_usage() -> None:
+    """Test the bias is initialized and used correctly."""
+    layer = ConstrainedUnitNormLinear(learnt_features=3, decoded_features=4, bias=True)
+    assert layer.bias is not None
+    # Check the bias is used in the forward pass
+    input_tensor = torch.zeros(5, 3)
+    output = layer(input_tensor)
+    assert torch.allclose(output, layer.bias.unsqueeze(0).expand(5, -1))
+
+
+def test_multiple_training_steps() -> None:
+    """Test the unit norm constraint over multiple training iterations."""
+    torch.random.manual_seed(0)
+    layer = ConstrainedUnitNormLinear(learnt_features=3, decoded_features=4)
+    optimizer = torch.optim.Adam(layer.parameters(), lr=0.01)
+    for _ in range(10):
+        data = torch.randn(5, 3)
+        optimizer.zero_grad()
+        logits = layer(data)
+
+        weight_norms = torch.norm(layer.weight, dim=1)
+        assert torch.allclose(weight_norms, torch.ones_like(weight_norms), atol=2e-3)
+
+        loss = torch.mean(logits**2)
+        loss.backward()
+        optimizer.step()
+
+
 def test_unit_norm_applied_backward() -> None:
     """Check that the unit norm is applied after each gradient step."""
     torch.random.manual_seed(0)
