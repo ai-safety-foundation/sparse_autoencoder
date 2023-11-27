92451c18686af08ef57254b8a27ad83c5edad977
HoagyC <hoagycunningham@gmail.com>
2023-11-27 12:56:52 +0000

    Test working.

--- sparse_autoencoder/metrics/validate/perplexity_metric.py
+++ sparse_autoencoder/metrics/validate/perplexity_metric.py
@@ -6,11 +6,13 @@ import torch
 from torch import Tensor
 from transformer_lens.hook_points import HookPoint
 
-from sparse_autoencoder.autoencoder.abstract_autoencoder import \
-    AbstractAutoencoder
+from sparse_autoencoder.autoencoder.abstract_autoencoder import AbstractAutoencoder
 from sparse_autoencoder.metrics.validate.abstract_validate_metric import (
-    AbstractValidationMetric, ValidationMetricContext)
+    AbstractValidationMetric,
+    ValidationMetricContext,
+)
 from sparse_autoencoder.train.utils import get_model_device
+
 
 N_BATCHES = 100
 
@@ -43,16 +45,10 @@ class PerplexityMetric(AbstractValidationMetric):
 
         with torch.no_grad():
             for batch_raw in context.dataset:
-                batch = torch.tensor(batch_raw["input_ids"]).to(device=device).to(torch.int64)[:2]
-                print(batch.shape)
+                batch = torch.tensor(batch_raw["input_ids"]).to(device=device).to(torch.int64)
 
                 # Get the activations from the model
                 sae_hook = make_autoencoder_hook(context.autoencoder)
-                # standard_loss2 = context.source_model.run_with_hooks(batch, return_type="loss")
-                # sae_loss2 = context.source_model.run_with_hooks(
-                #     batch, return_type="loss", fwd_hooks=[(context.hook_point, sae_hook)]
-                # )
-
                 standard_logits = context.source_model.run_with_hooks(batch, return_type="logits")
                 sae_logits = context.source_model.run_with_hooks(
                     batch, return_type="logits", fwd_hooks=[(context.hook_point, sae_hook)]
@@ -66,22 +62,17 @@ class PerplexityMetric(AbstractValidationMetric):
                 kl_divergence = kl_divergence / batch.shape[0]
                 # loss should be equal to the negative log likelihood
                 sae_logit_outputs = sae_logits[:, :-1].reshape(-1, sae_logits.shape[-1])
-                standard_logit_outputs = standard_logits[:, :-1].reshape(-1, standard_logits.shape[-1])
-                # sae_log_probs = torch.nn.functional.log_softmax(sae_logit_outputs, dim=-1)
-                standard_log_probs = torch.nn.functional.log_softmax(standard_logit_outputs, dim=-1)
+                standard_logit_outputs = standard_logits[:, :-1].reshape(
+                    -1, standard_logits.shape[-1]
+                )
 
                 targets = batch[1:]
                 criterion = torch.nn.CrossEntropyLoss()
-                nll_loss = torch.nn.NLLLoss()
-                targets2 = torch.tensor(targets[0].item()).to(device=device).to(torch.int64).unsqueeze(0)
-                print(criterion(sae_logit_outputs, targets2))
-                print(criterion(sae_logit_outputs, targets))
 
-                sae_loss = criterion(sae_logit_outputs, targets)
-                standard_loss = torch.nn.functional.nll_loss(
-                    standard_log_probs, targets, reduction="mean"
-                )
-                breakpoint()
+                sae_loss = criterion(
+                    sae_logit_outputs, targets * 1
+                )  # multiplication by 1 fixes an unknown mps problem
+                standard_loss = criterion(standard_logit_outputs, targets * 1)
 
                 total_standard_loss += standard_loss.item()
                 total_sae_loss += sae_loss.item()
