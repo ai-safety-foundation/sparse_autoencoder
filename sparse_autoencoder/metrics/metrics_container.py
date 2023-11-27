0b3337b53324eb45b5c8c06c3d53e8c79726c289
Lucy Farnik <101606499+lucyfarnik@users.noreply.github.com>
2023-11-27 03:20:49 +0000

    Add learned activations L0 norm metric (#91)
    ---------
    
    Co-authored-by: Alan Cooney <41682961+alan-cooney@users.noreply.github.com>

--- sparse_autoencoder/metrics/metrics_container.py
+++ sparse_autoencoder/metrics/metrics_container.py
@@ -7,6 +7,7 @@ from sparse_autoencoder.metrics.resample.neuron_activity_metric import NeuronAct
 from sparse_autoencoder.metrics.train.abstract_train_metric import AbstractTrainMetric
 from sparse_autoencoder.metrics.train.capacity import CapacityMetric
 from sparse_autoencoder.metrics.train.feature_density import TrainBatchFeatureDensityMetric
+from sparse_autoencoder.metrics.train.l0_norm_metric import TrainBatchLearnedActivationsL0
 from sparse_autoencoder.metrics.validate.abstract_validate_metric import AbstractValidationMetric
 from sparse_autoencoder.metrics.validate.model_reconstruction_score import ModelReconstructionScore
 
@@ -32,7 +33,11 @@ class MetricsContainer:
 
 
 default_metrics = MetricsContainer(
-    train_metrics=[TrainBatchFeatureDensityMetric(), CapacityMetric()],
+    train_metrics=[
+        TrainBatchFeatureDensityMetric(),
+        CapacityMetric(),
+        TrainBatchLearnedActivationsL0(),
+    ],
     resample_metrics=[NeuronActivityMetric()],
     validation_metrics=[ModelReconstructionScore()],
 )
---
+++ sparse_autoencoder/metrics/train/l0_norm_metric.py
@@ -0,0 +1,25 @@
+"""L0 norm sparsity metric."""
+from typing import final
+
+import torch
+
+from sparse_autoencoder.metrics.train.abstract_train_metric import (
+    AbstractTrainMetric,
+    TrainMetricData,
+)
+
+
+@final
+class TrainBatchLearnedActivationsL0(AbstractTrainMetric):
+    """Learned activations L0 norm sparsity metric.
+
+    The L0 norm is the number of non-zero elements in a learned activation vector. We then average
+    this over the batch.
+    """
+
+    def calculate(self, data: TrainMetricData) -> dict[str, float]:
+        """Create a log item for Weights and Biases."""
+        batch_size = data.learned_activations.size(0)
+        n_non_zero_activations = torch.count_nonzero(data.learned_activations)
+        batch_average = n_non_zero_activations / batch_size
+        return {"learned_activations_l0_norm": batch_average.item()}
