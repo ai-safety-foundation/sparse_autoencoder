6d8523add2b63ad6ea8c5d489d0460e6a11c383b
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-12-16 07:19:59 -0300

    Add snapshot l2 tests

--- sparse_autoencoder/loss/tests/test_decoded_activations_l2.py
+++ sparse_autoencoder/loss/tests/test_decoded_activations_l2.py
@@ -1,7 +1,9 @@
 """Tests for the L2ReconstructionLoss class."""
 import pytest
+from syrupy.session import SnapshotSession
 import torch
 
+from sparse_autoencoder.autoencoder.utils.tensor_shape import shape_with_optional_dimensions
 from sparse_autoencoder.loss.decoded_activations_l2 import L2ReconstructionLoss
 
 
@@ -37,18 +39,45 @@ def test_l2_loss_with_zero_input(l2_loss: L2ReconstructionLoss) -> None:
     ), "L2 loss should be zero for identical zero inputs."
 
 
-def test_l2_loss_with_varying_input_shapes(l2_loss: L2ReconstructionLoss) -> None:
+@pytest.mark.parametrize(
+    ("input_shape", "learned_activations_shape"),
+    [
+        ((2, 3), (2, 6)),
+        ((2, 4, 3), (2, 4, 6)),
+    ],
+)
+def test_l2_loss_with_varying_input_shapes(
+    l2_loss: L2ReconstructionLoss,
+    input_shape: tuple[int],
+    learned_activations_shape: tuple[int],
+    snapshot: SnapshotSession,
+) -> None:
     """Test the L2 loss function with varying input shapes."""
-    for shape in [(1, 3), (5, 3), (10, 5)]:
-        input_activations = torch.rand(shape)
-        output_activations = torch.rand(shape)
-        learned_activations = torch.zeros_like(input_activations)
+    torch.manual_seed(0)
+    input_activations = torch.rand(shape_with_optional_dimensions(*input_shape))
+    output_activations = torch.rand(shape_with_optional_dimensions(*input_shape))
+    learned_activations = torch.rand(shape_with_optional_dimensions(*learned_activations_shape))
 
-        calculated_loss = l2_loss.forward(
-            input_activations, learned_activations, output_activations
-        )
+    calculated_loss = l2_loss.forward(input_activations, learned_activations, output_activations)
 
-        # Just checking if the loss calculation completes without error for different shapes
-        assert (
-            calculated_loss.shape[0] == shape[0]
-        ), f"L2 loss calculation failed for shape {shape}."
+    rounded_zd_loss = torch.round(calculated_loss * 1e5).to(dtype=torch.int).tolist()
+    assert str(rounded_zd_loss) == snapshot, "L2 loss has changed from the snapshot."
+
+
+def test_l2_same_no_components_vs_1_component(l2_loss: L2ReconstructionLoss) -> None:
+    """Test the L2 loss is the same with no components vs 1."""
+    torch.manual_seed(0)
+    input_activations = torch.rand(10, 3)
+    learned_activations = torch.rand(10, 5)
+    output_activations = torch.rand(10, 3)
+
+    calculated_loss = l2_loss.forward(input_activations, learned_activations, output_activations)
+    calculated_loss_1_component = l2_loss.forward(
+        input_activations.unsqueeze(1),
+        learned_activations.unsqueeze(1),
+        output_activations.unsqueeze(1),
+    )
+
+    assert torch.allclose(
+        calculated_loss, calculated_loss_1_component.squeeze(1)
+    ), "L2 loss should be the same with no components vs 1."
