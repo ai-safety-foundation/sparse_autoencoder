ba7e20f8cc720df6cc7f438f5c038ab863c6b605
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-12-22 16:40:24 -0300

    Add GPT2 wide search

---
+++ sparse_autoencoder/experiments/gpt2_wide_initial_sweep.py
@@ -0,0 +1,65 @@
+"""Initial sweep."""
+import os
+
+from sparse_autoencoder import (
+    AutoencoderHyperparameters,
+    Hyperparameters,
+    LossHyperparameters,
+    Method,
+    OptimizerHyperparameters,
+    Parameter,
+    PipelineHyperparameters,
+    SourceDataHyperparameters,
+    SourceModelHyperparameters,
+    SweepConfig,
+    sweep,
+)
+from sparse_autoencoder.train.sweep_config import ActivationResamplerHyperparameters
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+def main() -> None:
+    """Run the experiment."""
+    sweep_config = SweepConfig(
+        parameters=Hyperparameters(
+            loss=LossHyperparameters(
+                l1_coefficient=Parameter(min=5e-4, max=1e-2),
+            ),
+            optimizer=OptimizerHyperparameters(
+                lr=Parameter(min=1e-4, max=1e-3),
+            ),
+            source_model=SourceModelHyperparameters(
+                name=Parameter("gpt2"),
+                cache_names=Parameter(["blocks.0.hook_mlp_out"]),
+                hook_dimension=Parameter(1600),
+            ),
+            source_data=SourceDataHyperparameters(
+                dataset_path=Parameter("alancooney/sae-monology-pile-uncopyrighted-tokenizer-gpt2"),
+                context_size=Parameter(128),
+                pre_tokenized=Parameter(value=True),
+            ),
+            autoencoder=AutoencoderHyperparameters(expansion_factor=Parameter(values=[2, 4, 8])),
+            pipeline=PipelineHyperparameters(
+                max_activations=Parameter(100_000_000),
+                checkpoint_frequency=Parameter(25_000_000),
+                validation_frequency=Parameter(25_000_000),
+            ),
+            activation_resampler=ActivationResamplerHyperparameters(
+                resample_interval=Parameter(50_000_000),
+                n_activations_activity_collate=Parameter(25_000_000),
+                threshold_is_dead_portion_fires=Parameter(
+                    1e-6,
+                ),
+                max_n_resamples=Parameter(1),
+            ),
+        ),
+        method=Method.RANDOM,
+    )
+
+    sweep(sweep_config=sweep_config)
+
+
+if __name__ == "__main__":
+    main()
